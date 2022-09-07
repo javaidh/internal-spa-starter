9293b6d4202b742b25452a29452f7a29b0383332
Hassan Javaid <h.blade@gmail.com>
2022-09-07 17:40:41 -0500

    Added Mantine and created Demo splash page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { Styles } from "./App.styles";
+import * as React from "react";
+import { MantineProvider, Text } from "@mantine/core";
+import { theme } from "./styles/theme";
+import { Demo } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <div className={Styles.app}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={Styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={Styles.readDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+      <Demo />
+    </MantineProvider>
   );
-}
+};
 
 export default App;
